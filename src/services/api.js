f55641b631bc4048e7adc1bd415ac520b69c8ec1
Syed Ali Zaidi <s.m.kumailhyder@gmail.com>
2025-11-26 17:22:20 +0500

    BaseURL fixed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.3.81:8000',
+  baseURL: 'https://api.stellaeai.com',
   headers: {
     'Content-Type': 'application/json',
   },
